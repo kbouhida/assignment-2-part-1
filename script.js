9a1010d443cbb61d065bb26723fda3461a7b456b
oxcygen <bouhidakaoutar@icloud.com>
2017-02-22 22:54:03 -0800

    Assignment 2 part 1

---
+++ script.js
@@ -0,0 +1,42 @@
+//STEP 1
+//var yourName = prompt("What is your name?");
+//alert(yourName.length);
+
+//STEP 2
+//var yourName = prompt("What is your name?"); 
+//alert(yourName.charAt(2));
+
+//STEP 3
+//var firstName = prompt("What is your first name?");
+//var lastName = prompt("What is your last name?");
+//alert("Your name is: " + firstName + " " + lastName);
+
+//STEP 4    
+//var myVariable = ("The quick brown fox jumps over the lazy dog");
+//alert(myVariable.indexOf("fox"));
+
+//STEP 5
+//var myVariable = ("The quick brown fox jumps over the lazy dog");
+//alert(myVariable.lastIndexOf("fox"));
+
+//STEP 6
+//var myVariable = ("The quick brown fox jumps over the lazy dog");
+//var userName = prompt("What is your name?");
+//alert(myVariable.replace("the lazy dog", userName));
+
+//STEP 7
+//var myVariable = ("The quick brown fox jumps over the lazy dog");
+//var aWord = prompt("enter a word");
+//alert(aWord.search("Katie"));
+
+//STEP 8
+//var old_variable = ("The quick brown fox jumps over the lazy dog");
+//var new_variable = alert(old_variable.slice(31, 43).toLocaleUpperCase());
+
+//STEP 9
+//var upVariable = ("            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ");
+//alert(upVariable.trim().toLocaleLowerCase());
+
+//STEP 10
+//var myVariable = ("the quick brown fox jumps over the lazy dog");
+//alert(myVariable.charAt(0).toLocaleUpperCase() + myVariable.slice(1));
